ef9ff4fb8e466bb494d37a4161efd4fc961b2acc
Jorge de los Santos <jdelossantos@tribune.com>
2015-03-26 15:01:20 -0300

    Added topbar

--- app/models/mimo.rb
+++ app/models/mimo.rb
@@ -1,5 +1,19 @@
 class Mimo < ActiveRecord::Base
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+
+  enum size: [ :small, :medium, :big ]
+
   belongs_to :user
+
+  def show_size
+    case size
+    when "small"
+      "Pequeño"
+    when "medium"
+      "Mediano"
+    when "big"
+      "Grande"
+    end
+  end
 end
