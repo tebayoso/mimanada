b3d7ae827612b73d29897e4fa9d7e5ebc36069ac
Jorge de los Santos <tebayoso@gmail.com>
2015-04-06 11:18:01 -0300

    Added Readme

--- db/schema.rb
+++ db/schema.rb
@@ -13,47 +13,44 @@
 
 ActiveRecord::Schema.define(version: 20150401030135) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "mimos", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.integer  "mimo_case"
-    t.string   "description"
-    t.string   "race"
-    t.integer  "size"
-    t.string   "address"
-    t.string   "coordinates"
-    t.string   "name"
-    t.string   "color1"
-    t.string   "color2"
-    t.string   "color3"
-    t.string   "gender"
-    t.integer  "age"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
+    t.integer  "user_id",            limit: 4
+    t.string   "title",              limit: 255
+    t.integer  "mimo_case",          limit: 4
+    t.string   "description",        limit: 255
+    t.string   "race",               limit: 255
+    t.integer  "size",               limit: 4
+    t.string   "address",            limit: 255
+    t.string   "coordinates",        limit: 255
+    t.string   "name",               limit: 255
+    t.string   "color1",             limit: 255
+    t.string   "color2",             limit: 255
+    t.string   "color3",             limit: 255
+    t.string   "gender",             limit: 255
+    t.integer  "age",                limit: 4
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "photo_file_name",    limit: 255
+    t.string   "photo_content_type", limit: 255
+    t.integer  "photo_file_size",    limit: 4
     t.datetime "photo_updated_at"
-    t.integer  "status"
+    t.integer  "status",             limit: 4
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin"
+    t.boolean  "admin",                  limit: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
